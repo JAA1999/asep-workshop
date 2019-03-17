b0db97a50d16c54563a97e33c3a25cf799e0c817
Alasdair Russell <2315645r@student.gla.ac.uk>
2019-03-17 22:22:31 +0000

    Setup empty templates with basic views and mapped the URL's. Added basic links to base.html.

--- RNG/views.py
+++ RNG/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 def index(request):
     context_dict={}
     return render(request, 'RNG/index.html', context=context_dict)
+
+def about(request):
+    context_dict={}
+    return render(request, 'RNG/about.html', context=context_dict)
+
+def category(request):
+    context_dict={}
+    return render(request, 'RNG/category.html', context=context_dict)
+
+def signup(request):
+    context_dict={}
+    return render(request, 'RNG/signup.html', context=context_dict)
+
+def game(request):
+    context_dict={}
+    return render(request, 'RNG/game.html', context=context_dict)
