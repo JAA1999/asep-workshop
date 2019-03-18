cfffc27ba34a65f9b2a9e112fd6fb2591a1647af
Ross Johnstone <2325563j@student.gla.ac.uk>
2019-03-18 21:13:54 +0000

    category slug

--- RNG/urls.py
+++ RNG/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^category/', views.category, name='category'),
+    url(r'^category', views.category, name='category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_game/$', views.add_game, name='add_game'),
     url(r'^signup/$', views.signup, name='signup'),
 	url(r'^signin/$', views.user_login, name='signin'),
 	url(r'^signout/$', views.user_logout, name='signout'),
