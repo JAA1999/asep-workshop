3f23e04d3d1df7cfecccdd53fa983ee49afd5999
Jamie Allan <2316615a@student.gla.ac.uk>
2019-03-19 12:54:44 +0000

    remove static_root directory

--- RNG_project/settings.py
+++ RNG_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR= os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR= os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR,'static', 'static_root')
+STATIC_ROOT = STATIC_DIR
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_dirs'),)
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
