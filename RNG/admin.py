234c9d4e7275803674e2a649d1d81e43f91c00a4
2198998b <35694227+2198998b@users.noreply.github.com>
2019-03-19 14:24:17 +0000

    all models registered in admin

--- RNG/admin.py
+++ RNG/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from RNG.models import User, Game
+from RNG.models import User, Category, Game, Rating, Comment
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Category)
 admin.site.register(Game)
+admin.site.register(Rating)
+admin.site.register(Comment)
