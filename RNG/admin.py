e5b65477db17889957a0e00ab31eecc62f8dc7e9
2198998b <35694227+2198998b@users.noreply.github.com>
2019-03-19 14:05:01 +0000

    fixed admin.py to match models

--- RNG/admin.py
+++ RNG/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from RNG.models import Users, Game
+from RNG.models import User, Game
 from django.contrib.auth.models import User, Group
 
 
@@ -7,5 +7,5 @@ from django.contrib.auth.models import User, Group
 
 # Register your models here.
 
-admin.site.register(Users)
+admin.site.register(User)
 admin.site.register(Game)
