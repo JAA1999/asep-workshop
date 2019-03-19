e8db321c52b114ac63ce93d3227bfb45e88f8953
Ross Johnstone <2325563j@student.gla.ac.uk>
2019-03-19 16:58:28 +0000

    popscript2

--- populate_rng.py
+++ populate_rng.py
@@ -6,21 +6,21 @@ from RNG.models import Category, Game, Rating, User
 def populate():
     #creates a list of dictionaries containing games to add into each category
     action=[
-        {"ID": "AC2",
+        {"id": "AC2",
         "name": "Assassin's Creed 2",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 18},
-        {"ID": "GTAV",
+        {"is": "GTAV",
         "name": "Grand Theft Auto V",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 18},
-        {"ID": "DISH",
+        {"id": "DISH",
         "name": "Dishonored",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -29,21 +29,21 @@ def populate():
         "age_rating": 18}
     ]
     rpg=[
-        {"ID": "SKY",
+        {"id": "SKY",
         "name": "Skyrim",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 15},
-        {"ID": "ME2",
+        {"id": "ME2",
         "name": "Mass Effect 2",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 15},
-        {"ID": "DAI",
+        {"id": "DAI",
         "name": "Dragon Age: Inquisition",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -52,21 +52,21 @@ def populate():
         "age_rating": 18}
     ]
     strategy=[
-        {"ID": "DOTA2",
+        {"id": "DOTA2",
         "name": "DOTA 2",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 16},
-        {"ID": "CIV5",
+        {"id": "CIV5",
         "name": "Civilization V",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 12},
-        {"ID": "SPORE",
+        {"id": "SPORE",
         "name": "Spore",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -75,21 +75,21 @@ def populate():
         "age_rating": 12}
     ]
     puzzle=[
-        {"ID": "PORTAL2",
+        {"id": "PORTAL2",
         "name": "Portal 2",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 12},
-        {"ID": "TTP",
+        {"id": "TTP",
         "name": "The Talos Principle",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 12},
-        {"ID": "THEWITNESS",
+        {"id": "THEWITNESS",
         "name": "The Witness",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -98,21 +98,21 @@ def populate():
         "age_rating": 12}
     ]
     sports=[
-        {"ID": "ROCKETLEAGUE",
+        {"id": "ROCKETLEAGUE",
         "name": "Rocket League",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 3},
-        {"ID": "FIFA19",
+        {"id": "FIFA19",
         "name": "Fifa 19",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 3},
-        {"ID": "NBA2K19",
+        {"id": "NBA2K19",
         "name": "NBA 2K19",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -121,21 +121,21 @@ def populate():
         "age_rating": 3}
     ]
     mmo=[
-        {"ID": "WoW",
+        {"id": "WoW",
         "name": "World Of Warcraft",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 12},
-        {"ID": "ESO",
+        {"id": "ESO",
         "name": "Elder Scrolls Online",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 12},
-        {"ID": "GW2",
+        {"id": "GW2",
         "name": "Guild Wars 2",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -144,21 +144,21 @@ def populate():
         "age_rating": 12}
     ]
     simulation=[
-        {"ID": "SIMS4",
+        {"id": "SIMS4",
         "name": "The Sims 4",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 3},
-        {"ID": "KSP",
+        {"id": "KSP",
         "name": "Kerbal Space Program",
         "user_score": 5,
         "num_user_ratings": 5,
         "critic_score": 5,
         "num_critic_ratings": 5,
         "age_rating": 3},
-        {"ID": "FS19",
+        {"id": "FS19",
         "name": "Farming Simulator 19",
         "user_score": 5,
         "num_user_ratings": 5,
@@ -177,28 +177,24 @@ def populate():
     }
 
     for cat, cat_data in cats.items():
-        c=add_cat(cat, cats[cat]["reviews"])
+        c=add_cat(cat, cats[cat]["views"]["likes"])
         for p in cat_data["games"]:
-            add_game(c,p["name"],p["url"],p["reviews"])
+            add_game(c,p["name"],p["url"],p["views"])
     for c in Category.objects.all():
         for p in Game.objects.filter(category=c):
             print("- {0} - {1}".format(str(c),str(p)))
 
-    def add_game(cat, title, url, reviews=0):
-        p = Game.objects.get_or_create(category=cat,ID=ID)
-        p.name=name
-        p.user_score=user_score
-        p.num_user_ratings=num_user_ratings
-        p.critic_score=critic_score
-        p.num_critic_ratings=num_critic_ratings
-        p.age_rating=age_rating
-        p.reviews=reviews
+    def add_game(cat, title, url, views):
+        p = Game.objects.get_or_create(category=cat,title=title)[0]
+        p.url = url
+        p.views = views
         p.save()
         return p
 
-    def add_cat(name,views):
+    def add_cat(name,views,likes):
         c=Category.objects.get_or_create(name=name)[0]
         c.views=views
+        c.likes=likes
         c.save()
         return c
 
