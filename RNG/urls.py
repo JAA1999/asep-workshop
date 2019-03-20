47c9750468f0a7e3a41051796de57e01ec154c5a
Jason Cook <2328261c@student.gla.ac.uk>
2019-03-20 16:59:39 +0000

    fixed a url

--- RNG/urls.py
+++ RNG/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
 	url(r'^signin/$', views.user_login, name='signin'),
 	url(r'^signout/$', views.user_logout, name='signout'),
-    url(r'^game', views.signup, name='game'),
+    url(r'^game', views.game, name='game'),
     #url(r'^search', views.search, name='search'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
